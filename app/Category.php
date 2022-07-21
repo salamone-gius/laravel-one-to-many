2710e6867598267203c5378546e952492a6727a8
salamone-gius <salamone.gius@gmail.com>
2022-07-21 19:28:16 +0200

    relationship through models created

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // creo un metodo pubblico che si chiama come la tabella dipendente
+    public function posts() {
+
+        // traduzione: restituisci $questoModel(una singola categoria)->ha molti('il Model legato') (Post)
+        return $this->hasMany('App\Post');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,11 @@ class Post extends Model
 {
     // imposto le condizioni per il mass assignment (protezione dei campi)
     protected $guarded = [];
+
+    // creo un metodo pubblico che si chiama come la tabella principale (al singolare in caso di relazione uno a molti)
+    public function category() {
+
+        // traduzione: restituisci $questoModel(un singolo post)->appartine a('il Model legato') (una categoria)
+        return $this->belongsTo('App\Category');
+    }
 }
